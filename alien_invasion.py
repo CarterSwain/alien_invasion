2b0206539f302971027d7306164535ba0afea5c1
Your Name <your.email@example.com>
2025-02-18 17:09:29 -0600

    Set background-color in AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
         
+        # Set background color.
+        self.bg_color = (230, 230, 230)
+        
     def run_game(self):
         """ Start the main loop for the game. """
         while True:
@@ -21,8 +24,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                     
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)        
                     
-            # Make the most recently drawn screen visible
+                    
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
             
