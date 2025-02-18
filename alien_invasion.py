b935bbdce889cb7c07b5d5258b7e7a18af62483e
Your Name <your.email@example.com>
2025-02-18 17:04:57 -0600

    Define clock in AlienInvasion class to control game frame rate on multiple systems

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
 
-import pygame # type: ignore
+import pygame 
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior. """
@@ -9,6 +9,7 @@ class AlienInvasion:
         """ Initialize game, and create game resources. """
         pygame.init()
         
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
         
@@ -23,6 +24,7 @@ class AlienInvasion:
                     
             # Make the most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
             
 if __name__ == '__main__':
     # Make a game instance, and run the game.
