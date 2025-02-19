0bd93312744501445465cb101032cb4486433ba7
Your Name <your.email@example.com>
2025-02-18 20:05:21 -0600

    Adjust ship's speed by adding ship_speed attribute to Settings class

--- settings.py
+++ settings.py
@@ -3,8 +3,12 @@ class Settings:
     
     def __init__(self):
         """ Initialize game's settings. """
+        
         # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (46, 42, 146)
+        
+        # Ship settings.
+        self.ship_speed = 1.5
         
