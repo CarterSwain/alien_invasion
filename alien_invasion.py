f80e0835a19500b2da04d4399b13e63d94236be4
Your Name <your.email@example.com>
2025-02-20 15:15:19 -0600

    Allow game to start with Play Button and reset after player loses. Hide mouse cursor when game active.

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,6 +63,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)    
                 
     def _check_keydown_events(self, event):
         """ Respond to key presses."""
@@ -81,6 +84,25 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+            
+    def _check_play_button(self, mouse_pos):
+        """ Start a new game when the player clicks Play. """
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            # Reset the game statistics
+            self.stats.reset_stats()
+            self.game_active = True 
+            
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+            
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+            
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)       
             
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group. """
@@ -128,7 +150,8 @@ class AlienInvasion:
             # Pause.
             sleep(0.5)
         else:
-            self.game_active = False             
+            self.game_active = False
+            pygame.mouse.set_visible(True)             
             
     def _update_aliens(self):
         """ Check if the fleet is at an edge, then update positions. """
