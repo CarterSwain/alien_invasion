ebbe7c6a2d74065621e703fe976981e0829697be
Your Name <your.email@example.com>
2025-02-21 14:03:34 -0600

    Create Scoreboard class to hold values for player's collision between bullets and aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -24,8 +25,10 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
         
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics,
+        # and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -68,6 +71,7 @@ class AlienInvasion:
         """ Start new game by resetting stats and clearing Play. """ 
         # Reset the game statistics
         self.stats.reset_stats()
+        self.sb.prep_score()
         self.game_active = True 
             
         # Get rid of any remaining bullets and aliens.
@@ -171,7 +175,11 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided
         collisions = pygame.sprite.groupcollide(
                  self.bullets, self.aliens, False, True) # If no longer wanting a bullet that deletes anything in path, change first boolean arg to 'True' to delete bullet)
-        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            
         if not self.aliens:
             # Destroying existing bullets and create new fleet.
             self.bullets.empty()
@@ -282,6 +290,9 @@ class AlienInvasion:
         
         # Draw stars.
         self.stars.draw(self.screen)
+        
+        # Draw the score information.
+        self.sb.show_score()
         
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
---
+++ scoreboard.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Scoreboard:
+    """ A class to report scoring information. """
+    def __init__(self, ai_game):
+        """ Initialize scoring attributes. """
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+        
+        # Font settings for scoring information.
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+        
+        # Prepare the initial score image.
+        self.prep_score()
+        
+        
+    def prep_score(self):
+        """ Turn the score into a rendered image. """
+        score_str = str(self.stats.score) # Turn the numerical value of stats into a string 
+        self.score_image = self.font.render(score_str, True,
+                self.text_color, self.settings.bg_color) # We pass the score_str to Render which creates image.
+        
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20 # Place score image's right edge 20 pixels from right edge of screen.
+        self.score_rect.top = 20 # Place score image's top edge 20 pixels down from top of screen.
+        
+        
+    def show_score(self): 
+        """ Draw score to the screen. """
+        self.screen.blit(self.score_image, self.score_rect) # Draw score image at location that score.rect specifies.       
